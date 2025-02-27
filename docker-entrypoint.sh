da663d10ed2874ae98eb8164edc1b145b2228279
Ferenc Szontágh <szf@fsociety.hu>
2025-02-27 09:44:14 +0100

    separate migration and build status check

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,24 +1,28 @@
 #!/bin/sh
 set -e  # Exit immediately if any command fails
 
-# Check if migration and build have already been executed
+# Check if migration have already been executed
 if [ ! -f "/app/.migrated" ]; then
   echo "Running database migrations..."
   if ! npx drizzle-kit migrate; then
     echo "Migration failed. Exiting..."
     exit 1
   fi
-  echo "Migrations completed."
 
+  echo "Migrations completed."
+  touch /app/.migrated
+fi;
+
+# Check if the application has already been built
+if [ ! -f "/app/.built" ]; then
   echo "Building the application..."
   if ! npm run build; then
     echo "Build failed. Exiting..."
     exit 1
   fi
-  echo "Build completed."
 
-  # Create a marker file to indicate migrations and build are done
-  touch /app/.migrated
+  echo "Build completed."
+  touch /app/.built
 fi
 
 
